57057f4c7c4c1cc9449007a99c3b3f2e3952c69b
satishs248 <satishs248@gmail.com>
2021-07-02 08:56:07 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Satish Singh',                   # Type in your name
   author_email = 'satishs248@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/satishs248/fisrtPyPiPackageSatishS248/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['test', 'hello-world'],   # Keywords that define your package best
   install_requires=[ ],
   classifiers=[
